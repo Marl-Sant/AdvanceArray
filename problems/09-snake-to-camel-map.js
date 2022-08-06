fe4e10a2ad64d028ef649e2acffbc00d34d868d3
Marlon Santos <santosfmarlon@gmail.com>
2022-08-06 15:28:03 -0400

    prob 9

--- problems/09-snake-to-camel-map.js
+++ problems/09-snake-to-camel-map.js
@@ -16,7 +16,13 @@ console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
 */
 
 function snakeToCamel(str) {
-    // Your code here
+    let words = str.split("_")
+    let capWords = words.map(function (capital) {
+
+        return capital[0].toUpperCase() + capital.slice(1).toLowerCase()
+
+    })
+    return capWords.join("")
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -25,4 +31,4 @@ try {
     module.exports = snakeToCamel;
 } catch (e) {
     module.exports = null;
-}
+}
