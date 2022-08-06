5d65cc9d5d629b0dda29e7ab73ebf1878aa7c3e4
Kelly Kong <kkong1992@gmail.com>
2022-08-06 16:15:29 -0400

    problem 16

--- problems/15-choose-primes.js
+++ problems/15-choose-primes.js
@@ -20,10 +20,25 @@ console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
 
 let isPrime = function(num) {
     // Your code here
+if (num < 2) return false
+for (let i = 2; i < num; i++){
+    if (num % i === 0){
+        return false
+    }
+}
+return true
 }
 
 let choosePrimes = function(nums) {
     // Your code here
+    let newArr = []
+    nums.forEach(function(ele){
+        let primeCheck = isPrime(ele)
+        if (primeCheck === true){
+            newArr.push(ele)
+        }
+    })
+    return newArr
 }
 
 // alternative solution using Array.filter
@@ -37,4 +52,4 @@ try {
     module.exports = choosePrimes;
 } catch (e) {
     module.exports = null;
-}
+}
