cf6c5af0df8cd97f1f65a4c36a65727f94a820ee
Kelly Kong <kkong1992@gmail.com>
2022-08-06 12:35:00 -0400

    prob 2

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -15,6 +15,14 @@ console.log(avgVal([])); // null
 
 let avgVal = function(arr) {
     // Your code here
+    let avg = 0
+    if (arr.length === 0) {
+      return null
+    }
+    arr.forEach(ele => {
+      avg += ele
+    });
+    return avg / arr.length
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
