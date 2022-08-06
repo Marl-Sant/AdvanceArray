9a6ac67b28903bc5ddc5731eef80e523e02ab904
Kelly Kong <kkong1992@gmail.com>
2022-08-06 16:07:21 -0400

    problem 13

--- problems/13-unique.js
+++ problems/13-unique.js
@@ -16,6 +16,13 @@ console.log(unique(['a', 'b', 'c', 'b'])); // ['a', 'b', 'c']
 
 let unique = function(array) {
     // Your code here
+    let newArr = []
+    array.forEach(function(ele){
+        if (!newArr.includes(ele)){
+            newArr.push(ele)
+        }
+    })
+    return newArr
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -24,4 +31,4 @@ try {
     module.exports = unique;
 } catch (e) {
     module.exports = null;
-}
+}
